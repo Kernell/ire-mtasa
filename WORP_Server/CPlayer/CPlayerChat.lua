09646d1650cfb6718b0463c9910a2a711caae13b
Kernell <kernell.it@gmail.com>
2014-01-18 20:27:25 +0400

    Рефакторинг CPlayerChat

--- WORP_Server/CPlayer/CPlayerChat.lua
+++ WORP_Server/CPlayer/CPlayerChat.lua
@@ -1,46 +1,93 @@
-﻿-- Innovation Roleplay Engine
+-- Innovation Roleplay Engine
 --
 -- Author		Kernell
 -- Copyright	© 2011 - 2014
 -- License		Proprietary Software
 -- Version		1.0
 
-class "CPlayerChat"
+class: CPlayerChat
+{
+	CPlayerChat		= function( this, pPlayer )
+		this.m_pPlayer = pPlayer;
+	end;
 
-function CPlayerChat:CPlayerChat( pPlayer )
-	self.m_pPlayer = pPlayer;
-end
-
-function CPlayerChat:_CPlayerChat()
-	self.m_pPlayer = NULL;
-end
-
-function CPlayerChat:Send( sMessage, iRed, iGreen, iBlue, bColorCoded )
-	if classname( self ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
+	_CPlayerChat		= function()
+		this.m_pPlayer = NULL;
+	end;
 	
-	return outputChatBox( sMessage, self.m_pPlayer, tonumber( iRed ) or 255, tonumber( iGreen ) or 164, tonumber( iBlue ) or 0, bColorCoded == NULL and true or (bool)(bColorCoded) );
+	OnChat		= function( this, sMessage, iType )
+		if this.m_pPlayer:IsMuted() then
+			this:Send( "У Вас молчанка, Вы не можете говорить", 255, 128, 0 );
+			
+			return;
+		end
+		
+		if this.m_pPlayer:IsDead() then
+			return;
+		end
+		
+		if iType == 0 then
+			this.m_pPlayer:LocalizedMessage( sMessage );
+		elseif iType == 1 then
+			this.m_pPlayer:Me( sMessage, NULL, true, true );
+		elseif iType == 2 then
+			local pChar = this.m_pPlayer:GetChar();
+			
+			if pChar then
+				
+				
+				-- this.m_pPlayer:Me( this.m_pPlayer:Gender( "сказал", "сказала" ) + " что-то по рации", NULL, false, true );
+			end
+		end
+	end;
+
+	Send		= function( this, sMessage, iRed, iGreen, iBlue, bColorCoded )
+		if classname( this ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
+		
+		return outputChatBox( sMessage, this.m_pPlayer, tonumber( iRed ) or 255, tonumber( iGreen ) or 164, tonumber( iBlue ) or 0, bColorCoded == NULL and true or (bool)(bColorCoded) );
+	end;
+
+	Show		= function( this )
+		if classname( this ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
+		
+		return showChat( this.m_pPlayer, true );
+	end;
+
+	Hide		= function( this )
+		if classname( this ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
+		
+		return showChat( this.m_pPlayer.__instance, false );
+	end;
+
+	Error		= function( this, sText, ... )
+		if classname( this ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
+		
+		return this:Send( "Error: " + ( ( ... ) and sText:format( ... ) or sText ), 255, 0, 0, true );
+	end;
+
+	Warning		= function( this, sText, ... )
+		if classname( this ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
+		
+		return this:Send( "Warning: " + ( ( ... ) and sText:format( ... ) or sText ), 255, 128, 0, true );
+	end;
+};
+
+local function OnPlayerChat( sMessage, iType )
+	cancelEvent();
+	
+	if sMessage:gsub( " ", "" ):len() == 0 then
+		return false;
+	end
+	
+	if source:GetChat() then
+		source:GetChat():OnChat( sMessage, iType );
+	end
 end
 
-function CPlayerChat:Show()
-	if classname( self ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
-	
-	return showChat( self.m_pPlayer, true );
-end
+addEventHandler( "onPlayerChat", root, OnPlayerChat );
 
-function CPlayerChat:Hide()
-	if classname( self ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
-	
-	return showChat( self.m_pPlayer.__instance, false );
-end
-
-function CPlayerChat:Error( sText, ... )
-	if classname( self ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
-	
-	return self:Send( "Error: " + ( ( ... ) and sText:format( ... ) or sText ), 255, 0, 0, true );
-end
-
-function CPlayerChat:Warning( sText, ... )
-	if classname( self ) ~= 'CPlayerChat' then error( TEXT_E2288, 2 ) end
-	
-	return self:Send( "Warning: " + ( ( ... ) and sText:format( ... ) or sText ), 255, 128, 0, true );
-end
+addEventHandler( "onPlayerPrivateMessage", root, 
+	function()
+		cancelEvent();
+	end
+);
