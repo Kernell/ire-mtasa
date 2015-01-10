aa1a7ef650b5e3b7957ef148f68df6fe2e226320
Dmitry Korolev <kernell.it@gmail.com>
2015-01-10 19:39:06 +0300

    Временная заглушка

---
+++ IRE_Client/UIRadialMenu.lua
@@ -0,0 +1,19 @@
+-- Innovation Roleplay Engine
+--
+-- Author		Kernell
+-- Copyright	© 2011 - 2015
+-- License		Proprietary Software
+-- Version		1.0
+
+class. UIRadialMenu
+{
+	Visible			= false;
+	
+	UIRadialMenu	= function()
+	
+	end;
+	
+	_UIRadialMenu	= function()
+	
+	end;
+};
