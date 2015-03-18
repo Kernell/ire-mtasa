30ea508ac8ae09b8ee33d312337c6715af092e50
Kernell <kernell.it@gmail.com>
2015-03-18 14:52:05 +0300

    Реестр моделей, дабы избавиться от прямого указания ID при создании объекта. Экспортированная функция (клиент,сервер) GetInfo принимает строку в виде пути, например "weapons\acr" и возвращает всю информацию о arc (сейчас это ID, путь к DFF и TXD если есть)

---
+++ IRE_Models/registry.lua
@@ -0,0 +1,259 @@
+-- Innovation Roleplay Engine
+--
+-- Author		Kernell
+-- Copyright	© 2011 - 2015
+-- License		Proprietary Software
+-- Version		1.0
+
+Registry = 
+{
+	peds		=
+	{
+		wfyclot		=
+		{
+			ID		= 211;
+			DFF		= "Peds/wfyclot.dff";
+			TXD		= "Peds/wfyclot.txd";
+		};
+		gungrl		=
+		{
+			ID		= 191;
+			DFF		= "Peds/gungrl3.dff";
+			TXD		= "Peds/gungrl3.txd";
+		};
+		mecgrl		=
+		{
+			ID		= 192;
+			DFF		= "Peds/mecgrl3.dff";
+			TXD		= "Peds/mecgrl3.txd";
+		};
+		nurgrl		=
+		{
+			ID		= 193;
+			DFF		= "Peds/nurgrl3.dff";
+			TXD		= "Peds/nurgrl3.txd";
+		};
+		crogrl3		=
+		{
+			ID		= 194;
+			DFF		= "Peds/crogrl3.dff";
+			TXD		= "Peds/crogrl3.txd";
+		};
+		lapd		=
+		{
+			ID		= 280;
+			DFF		= "Peds/lapd.dff";
+			TXD		= "Peds/lapd.txd";
+		};
+		sfpd		=
+		{
+			ID		= 281;
+			DFF		= "Peds/sfpd.dff";
+			TXD		= "Peds/sfpd.txd";
+		};
+		lvpd		=
+		{
+			ID		= 282;
+			DFF		= "Peds/lvpd.dff";
+			TXD		= "Peds/lvpd.txd";
+		};
+		swat		=
+		{
+			ID		= 285;
+			DFF		= "Peds/urban.dff";
+			TXD		= "Peds/swat.txd";
+		};
+		fbi			=
+		{
+			ID		= 286;
+			DFF		= "Peds/fbi.dff";
+			TXD		= "Peds/fbi.txd";
+		};
+		fbi_swat	=
+		{
+			ID		= 287;
+			DFF		= "Peds/urban.dff";
+			TXD		= "Peds/fbi_swat.txd";
+		};
+		fbi_swat	=
+		{
+			ID		= 288;
+			DFF		= "Peds/urban.dff";
+			TXD		= "Peds/army.txd";
+		};
+	};
+	weapons		=
+	{
+		acr		= 
+		{
+			ID		= 1851;
+			DFF		= "Weapons/acr.dff";
+			TXD		= "Weapons/acr.txd";
+		};
+		ak74m		= 
+		{
+			ID		= 1877;
+			DFF		= "Weapons/ak74m.dff";
+			TXD		= "Weapons/ak74m.txd";
+		};
+		ak12		= 
+		{
+			ID		= 1852;
+			DFF		= "Weapons/ak12.dff";
+			TXD		= "Weapons/ak12.txd";
+		};
+		aug		= 
+		{
+			ID		= 1853;
+			DFF		= "Weapons/aug.dff";
+			TXD		= "Weapons/aug.txd";
+		};
+		desert_eagle		= 
+		{
+			ID		= 1875;
+			DFF		= "Weapons/desert_eagle.dff";
+			TXD		= "Weapons/desert_eagle.txd";
+		};
+		famas_f1		= 
+		{
+			ID		= 1854;
+			DFF		= "Weapons/famas_f1.dff";
+			TXD		= "Weapons/famas_f1.txd";
+		};
+		scar_h		= 
+		{
+			ID		= 1855;
+			DFF		= "Weapons/scar_h.dff";
+			TXD		= "Weapons/scar_h.txd";
+		};
+		scar_l		= 
+		{
+			ID		= 1856;
+			DFF		= "Weapons/scar_l.dff";
+			TXD		= "Weapons/scar_l.txd";
+		};
+		spas12		= 
+		{
+			ID		= 1857;
+			DFF		= "Weapons/spas12.dff";
+			TXD		= "Weapons/spas12.txd";
+		};
+		glock17		= 
+		{
+			ID		= 1858;
+			DFF		= "Weapons/glock17.dff";
+			TXD		= "Weapons/glock17.txd";
+		};
+		glock18		= 
+		{
+			ID		= 1859;
+			DFF		= "Weapons/glock18.dff";
+			TXD		= "Weapons/glock18.txd";
+		};
+		hk416		= 
+		{
+			ID		= 1860;
+			DFF		= "Weapons/hk416.dff";
+			TXD		= "Weapons/hk416.txd";
+		};
+		hk417		= 
+		{
+			ID		= 1861;
+			DFF		= "Weapons/hk417.dff";
+			TXD		= "Weapons/hk417.txd";
+		};
+		g36c		= 
+		{
+			ID		= 1862;
+			DFF		= "Weapons/g36c.dff";
+			TXD		= "Weapons/g36c.txd";
+		};
+		jng90		= 
+		{
+			ID		= 1864;
+			DFF		= "Weapons/jng90.dff";
+			TXD		= "Weapons/jng90.txd";
+		};
+		m4a1		= 
+		{
+			ID		= 1865;
+			DFF		= "Weapons/m4a1.dff";
+			TXD		= "Weapons/m4a1.txd";
+		};
+		m9		= 
+		{
+			ID		= 1866;
+			DFF		= "Weapons/m9.dff";
+			TXD		= "Weapons/m9.txd";
+		};
+		m1911		= 
+		{
+			ID		= 1867;
+			DFF		= "Weapons/m1911.dff";
+			TXD		= "Weapons/m1911.txd";
+		};
+		mp443		= 
+		{
+			ID		= 1868;
+			DFF		= "Weapons/mp443.dff";
+			TXD		= "Weapons/mp443.txd";
+		};
+		mp5		= 
+		{
+			ID		= 1863;
+			DFF		= "Weapons/mp5.dff";
+			TXD		= "Weapons/mp5.txd";
+		};
+		mp7		= 
+		{
+			ID		= 1876;
+			DFF		= "Weapons/mp7.dff";
+			TXD		= "Weapons/mp7.txd";
+		};
+		p90		= 
+		{
+			ID		= 1869;
+			DFF		= "Weapons/p90.dff";
+			TXD		= "Weapons/p90.txd";
+		};
+		qbu88		= 
+		{
+			ID		= 1870;
+			DFF		= "Weapons/qbu88.dff";
+			TXD		= "Weapons/qbu88.txd";
+		};
+		qjy88		= 
+		{
+			ID		= 1871;
+			DFF		= "Weapons/qjy88.dff";
+			TXD		= "Weapons/qjy88.txd";
+		};
+		rpg		= 
+		{
+			ID		= 1872;
+			DFF		= "Weapons/rpg.dff";
+			TXD		= "Weapons/rpg.txd";
+		};
+		rpk74m		= 
+		{
+			ID		= 1873;
+			DFF		= "Weapons/rpk74m.dff";
+			TXD		= "Weapons/rpk74m.txd";
+		};
+		ump45		= 
+		{
+			ID		= 1874;
+			DFF		= "Weapons/ump45.dff";
+			TXD		= "Weapons/ump45.txd";
+		};
+	};
+};
+
+function GetInfo( path )
+	path = path:split( "\\" );
+
+	local dir	= path[ 1 ];
+	local file	= path[ 2 ];
+
+	return Registry[ dir ] and Registry[ dir ][ file ];
+end
