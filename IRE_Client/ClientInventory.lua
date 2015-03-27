c0f719ac39dad265975f94525cf23cc7b658c935
Kernell <kernell.it@gmail.com>
2015-03-28 00:24:57 +0300

    Добавлены классы инвентаря клиента и менеджера предметов Позже допишу класс инвентаря (после того как появится хоть какой-то GUI)

---
+++ IRE_Client/ClientInventory.lua
@@ -0,0 +1,17 @@
+-- Innovation Roleplay Engine
+--
+-- Author		Kernell
+-- Copyright	© 2011 - 2015
+-- License		Proprietary Software
+-- Version		1.0
+
+class. ClientInventory
+{
+	ClientInventory	= function()
+		
+	end;
+
+	SyncItems		= function( items )
+		this.Items	= items;
+	end;
+}
